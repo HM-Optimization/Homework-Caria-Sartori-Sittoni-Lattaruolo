a24ef6040bccacc6b6e4ff40900a33f353e2865e
GianmarcoLattaruolo <87989501+GianmarcoLattaruolo@users.noreply.github.com>
2022-05-02 16:47:26 +0200

    Update f_deriv.m
    Secondo me questa è da ricontrollare...c'è qualcosa che non mi torna.

--- f_deriv.m
+++ f_deriv.m
@@ -1,4 +1,4 @@
-function[grad] = f_deriv(y,ybar,W,Wbar,u)
+function[grad] = f_deriv(y,y_samp,W,W_samp,u)
 
 grad = zeros([u,1]);
 sum1 = 0;
@@ -6,8 +6,8 @@ sum2 = 0;
 
 for j=1:u
 
-    sum1 = sum(W(:,j).*y(j,1)-W(:,j).*ybar);
-    sum2 = sum(Wbar(:,j).*y(j,1)-Wbar(:,j).*y); 
+    sum1 = sum( (W_samp(:,j)-W_samp(:,j)).*y_samp ) ;
+    sum2 = sum( W(:,j).*y(j,1)-W(:,j).*y); 
     grad(j,1) = sum1 + sum2;
 
 end
