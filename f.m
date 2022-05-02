8d828560de4985c5e6ea8881b20e73695ee9ca68
GianmarcoLattaruolo <87989501+GianmarcoLattaruolo@users.noreply.github.com>
2022-05-02 16:51:50 +0200

    Update f.m

--- f.m
+++ f.m
@@ -1,12 +1,12 @@
 % function f
 
-function[loss] = f(y, y_samp, W, Wbar)
+function [loss] = f(y, y_samp, W, W_samp)
 
 l = length(y_samp);
 u = length(y);
 
 loss=0;
 for j = 1:u
-    loss = loss + ((y_samp-y(j)).^2)'*W(:,j) + 0.5*Wbar(j,:)*(y-y(j)).^2;
+    loss = loss + ((y_samp-y(j)).^2)'*W_samp(:,j) + 0.5*W(j,:)*(y-y(j)).^2;
 end
 
