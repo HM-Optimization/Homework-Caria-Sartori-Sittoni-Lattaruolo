b5bad8f74433ab77e45438eb18641d58764f7d12
GianmarcoLattaruolo <87989501+GianmarcoLattaruolo@users.noreply.github.com>
2022-05-02 17:11:10 +0200

    Update BCGD_cyclic_fixed.m

--- BCGD_cyclic_fixed.m
+++ BCGD_cyclic_fixed.m
@@ -1,14 +1,15 @@
 % Cyclic BCGD method
 
-function[y] = BCGD_cyclic_fixed(alpha,y0,maxit,eps,ybar,W,Wbar,u)
+function [y] = BCGD_cyclic_fixed(alpha,y0,maxit,eps,y_samp,W,W_samp)
 
 % choose a starting point
 y = y0;
+u=len(y0);
 
 for k = 1:maxit
 
     % condition to stop
-    grad = f_deriv(y,ybar,W,Wbar,u);
+    grad = f_deriv(y,y_samp,W,W_samp);
     if norm(grad)<eps
         break
     end
@@ -17,8 +18,9 @@ for k = 1:maxit
 
     for i = 1:u
         U = zeros([u,1]);
-        U(i,1) = 1;
-        grad_z = f_deriv(z,ybar,W,Wbar,u);
+        U(i,1) = 1;  % con i blocchi di dimensione 1 questo vettore si può omettere?
+        % U.*grad_z = grad_z(i) ??
+        grad_z = f_deriv(z,y_samp,W,W_samp,u);
         z = z - alpha.*U.*grad_z;
 
     end
