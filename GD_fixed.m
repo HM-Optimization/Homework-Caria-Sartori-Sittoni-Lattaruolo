ca04db7bf6995e1ee987f3c768bf9139fa913877
GianmarcoLattaruolo <87989501+GianmarcoLattaruolo@users.noreply.github.com>
2022-05-02 16:43:07 +0200

    Update GD_fixed.m

--- GD_fixed.m
+++ GD_fixed.m
@@ -1,8 +1,8 @@
-function[y] = GD_fixed(alpha,y0,maxit,eps,ybar,W,Wbar,u)
+function[y] = GD_fixed(alpha,y0,maxit,eps,y_samp,W,W_samp,u)
 % Gradient descent per stepsize fisso
 y=y0;
 for i = 1:maxit
-    grad = f_deriv(y,ybar,W,Wbar,u);
+    grad = f_deriv(y,y_samp,W,W_samp,u);
   
     if norm(grad)<eps
         break
