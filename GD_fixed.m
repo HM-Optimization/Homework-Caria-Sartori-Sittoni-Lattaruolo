0decbf9fb745018b51492791785766efc78d6147
ir-ca <96775482+ir-ca@users.noreply.github.com>
2022-05-03 09:07:38 +0200

    Update GD_fixed.m

--- GD_fixed.m
+++ GD_fixed.m
@@ -1,4 +1,4 @@
-function[y] = GD_fixed(alpha,y0,maxit,eps,y_samp,W,W_samp,u)
+function[y, timeVec, Norms, accuracy] = GD_fixed(alpha,y0,maxit,eps,y_samp,W,W_samp,u)
 % Gradient descent per stepsize fisso
 y=y0;
 for i = 1:maxit
