ed264d48bcba6316f699e494fa5de19756f0616e
ir-ca <96775482+ir-ca@users.noreply.github.com>
2022-05-03 09:31:21 +0200

    Update armijo_rule.m

--- armijo_rule.m
+++ armijo_rule.m
@@ -1,12 +1,12 @@
 % Armijo Rule
 
-function [alpha] = armijo_rule(delta, gamma, grad, x, y_samp, W, W_samp)
+function [alpha] = armijo_rule(delta, gamma, grad, y, y_samp, W, W_samp)
 
 D = 10;
 m = 0;
-fx = f(x, y_samp, W, W_samp);
+fy = f(y, y_samp, W, W_samp);
 
-while f(x + delta^m*D*(-grad), y_samp, W, W_samp) > fx - gamma*delta^m*D*(grad'*grad)
+while f(y + delta^m*D*(-grad), y_samp, W, W_samp) > fy - gamma*delta^m*D*(grad'*grad)
     m = m+1;
 end 
 
