f16527e91774a5d06173fac659b62d024c46ac82
GianmarcoLattaruolo <87989501+GianmarcoLattaruolo@users.noreply.github.com>
2022-05-03 10:15:00 +0200

    Update GD_fixed.m

--- GD_fixed.m
+++ GD_fixed.m
@@ -1,15 +1,57 @@
-function[y, timeVec, Norms, accuracy] = GD_fixed(alpha,y0,maxit,eps,y_samp,W,W_samp,u)
+function [y, timeVec, Norms, accuracy] = GD_fixed(alpha,maxit,eps,y,y_samp,W,W_samp,step_size)
+%step_size = metodo per il calcolo dello step size che scegliamo
 % Gradient descent per stepsize fisso
-y=y0;
-for i = 1:maxit
-    grad = f_deriv(y,y_samp,W,W_samp,u);
-  
-    Norm = norm(grad);
-    Norms(k) = Norm;
-    
-    if Norm<eps
-        break
-    end
-    y = y - alpha.*grad;
-end
 
+Norms=zeros([1,maxit]);
+timeVec=0;
+accuracy=1;
+switch step_size
+
+    case 1  %alpha fisso
+
+        for i = 1:maxit
+            grad = f_deriv(y,y_samp,W,W_samp);
+  
+            Norm = norm(grad);
+            Norms(i) = Norm;
+            
+            if Norm<eps
+                break
+            end
+            y = y - alpha.*grad;
+        end
+    case 2  %armijo
+        for i=1:maxit
+
+            grad = f_deriv(y,y_samp,W,W_samp);
+  
+            Norm = norm(grad);
+            Norms(i) = Norm;
+            
+            if Norm<eps
+                break
+            end
+            delta=0.5;
+            alpha = armijo_rule(delta,grad,y,y_samp,W,W_samp);
+
+            y = y - alpha.*grad;
+        end
+
+
+    case  3 %exact line search
+        for i=1:maxit
+            grad = f_deriv(y,y_samp,W,W_samp);
+  
+            Norm = norm(grad);
+            Norms(i) = Norm;
+            
+            if Norm<eps
+                break
+            end
+
+            alpha = exact_line_search(y,y_samp,W,W_samp,grad);
+           
+            y = y - alpha.*grad;
+        end
+        
+end
