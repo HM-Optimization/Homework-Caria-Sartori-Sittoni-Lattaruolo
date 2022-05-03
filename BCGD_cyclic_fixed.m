a9e903bab0fa489048144f8861ebd2c9308f08b7
ir-ca <96775482+ir-ca@users.noreply.github.com>
2022-05-03 08:45:50 +0200

    Update BCGD_cyclic_fixed.m

--- BCGD_cyclic_fixed.m
+++ BCGD_cyclic_fixed.m
@@ -1,14 +1,15 @@
 % Cyclic BCGD method
 
-function[y] = BCGD_cyclic_fixed(alpha,y0,maxit,eps,ybar,W,Wbar,u)
+function[y] = BCGD_cyclic_fixed(alpha,y0,maxit,eps,y_samp,W,W_samp)
 
 % choose a starting point
 y = y0;
+u = length(y0);
 
 for k = 1:maxit
 
     % condition to stop
-    grad = f_deriv(y,ybar,W,Wbar,u);
+    grad = f_deriv(y,y_samp,W,W_samp);
     if norm(grad)<eps
         break
     end
@@ -17,7 +18,7 @@ for k = 1:maxit
 
     for i = 1:u
        
-        grad_z = f_deriv_block(z,ybar,W,Wbar,i);
+        grad_z = f_deriv_block(z,y_samp,W,W_samp,i);
         z(i) = z(i) - alpha.*grad_z;
 
     end
