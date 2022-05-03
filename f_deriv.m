897abbf7cda4fc5e8890a67b16cfee8439db4991
GianmarcoLattaruolo <87989501+GianmarcoLattaruolo@users.noreply.github.com>
2022-05-03 09:04:18 +0200

    Update f_deriv.m

--- f_deriv.m
+++ f_deriv.m
@@ -1,14 +1,14 @@
 function[grad] = f_deriv(y,y_samp,W,W_samp)
 
-u=len(y);
+u=length(y);
 grad = zeros([u,1]);
 sum1 = 0;
 sum2 = 0;
 
 for j=1:u
 
-    sum1 = sum( (W_samp(:,j)-W_samp(:,j)).*y_samp ) ;
-    sum2 = sum( W(:,j).*y(j,1)-W(:,j).*y); 
+    sum1 = sum( W_samp(:,j).*(y(j,1)-y_samp) );
+    sum2 = sum( W(:,j).*(y(j,1)-y) );
     grad(j,1) = sum1 + sum2;
 
 end
