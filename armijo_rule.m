2e338d95632e3a8927a8dba91d13ccfbee6b8bf2
GianmarcoLattaruolo <87989501+GianmarcoLattaruolo@users.noreply.github.com>
2022-05-06 15:45:05 +0200

    Update armijo_rule.m

--- armijo_rule.m
+++ armijo_rule.m
@@ -1,15 +1,19 @@
 % Armijo Rule
 function [alpha] = armijo_rule(delta, grad, y, y_samp, W, W_samp)
 
-gamma = 0.25;
-D = 1;
-m = 0;
-fx = f(y, y_samp, W, W_samp);
-
-while f(y + delta^m*D*(-grad), y_samp, W, W_samp) > fx - gamma*delta^m*D*(grad'*grad) & m<20
-    m = m+1;
-end 
-
-alpha = delta^m*D;
+if length(grad)~=1
+    gamma = 0.25;
+    D = 1;
+    m = 0;
+    fx = f(y, y_samp, W, W_samp);
+    
+    while f(y + delta^m*D*(-grad), y_samp, W, W_samp) > fx - gamma*delta^m*D*(grad'*grad) & m<20
+        m = m+1;
+    end 
+    
+    alpha = delta^m*D;
+else 
+    alpha = (1-gamma)/grad;
+end
 
 end
