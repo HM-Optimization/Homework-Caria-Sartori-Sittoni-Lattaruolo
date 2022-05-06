61a3e8d71bbcf0429e150a00ba754c3218594b11
GianmarcoLattaruolo <87989501+GianmarcoLattaruolo@users.noreply.github.com>
2022-05-06 15:44:09 +0200

    Update exact_line_search.m

--- exact_line_search.m
+++ exact_line_search.m
@@ -1,19 +1,20 @@
 %funzione per trovare l'alpha esatto rispetto alla loss che abbiamo
 function [alpha]=exact_line_search(y,y_samp,W,W_samp,grad)
 
-% grad è la direzione su cui stiamo cercando l'alpha ottimale
-% a partire da y
+if lenght(grad)~=1
 
-u=length(y);
-l=length(y_samp);
-num=0;
-den=0;
-for i=1:u
-    num = num + W_samp(:,i)'*(y(i)-y_samp)*grad(i) + W(i,:)*(y(i)-y)*grad(i);
-    den = den + sum(W_samp(:,i))*grad(i)^2 + W(i,:)*(grad(i)-grad)*grad(i);
+    u=length(y);
+    l=length(y_samp);
+    num=0;
+    den=0;
+    for i=1:u
+        num = num + W_samp(:,i)'*(y(i)-y_samp)*grad(i) + W(i,:)*(y(i)-y)*grad(i);
+        den = den + sum(W_samp(:,i))*grad(i)^2 + W(i,:)*(grad(i)-grad)*grad(i);
+    end
+    
+    alpha=num/den;
+else
+    alpha=1/(grad-1);
 end
 
-alpha=num/den;
-
 end
-
