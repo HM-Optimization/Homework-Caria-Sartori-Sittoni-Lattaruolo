946517665b3cafd641bc9f01fc0bd626ebd4fea3
GianmarcoLattaruolo <87989501+GianmarcoLattaruolo@users.noreply.github.com>
2022-05-07 14:25:01 +0200

    Update BCGD_random.m

--- BCGD_random.m
+++ BCGD_random.m
@@ -1,83 +1,90 @@
 function [y, timeVec, Norms, accuracy] = ...
-    BCGD_random(alpha,maxit,eps,y,y_samp,W,W_samp,step_size,y_exact,delta)
+    BCGD_random(maxit,eps,y,y_samp,W,W_samp,step_size,y_exact,delta)
 
-% choose a starting point
+maxit=100*maxit;
 u = length(y);
+for i=1:u
+    W_l(i) = sum(W_samp(:,i));
+    W_u(i) = sum(W(:,i));
+end
+C=W_l+W_u;
 
+Norms(1)=norm(f_deriv(y,y_samp,W,W_samp)); % we compute the norm ones every 50 iteration
 switch step_size
     case 1
-        for k = 1:maxit
-            tic;
+        lambda_max = max(eig(W));
+        L=sqrt(max(C)^2+lambda_max^2);
+        alpha=1/L;
 
-            % condition to stop
-            grad = f_deriv(y,y_samp,W,W_samp);
+        for k = 2:maxit+1
+            tic;
             
-            Norm = norm(grad);
-            Norms(k) = Norm;
+            % pick the block with uniform probability
+            i = randi([1 u]);
+            grad=f_deriv_block(y,y_samp,W,W_samp,i);
             
-            if Norm<eps
+            Norm = abs(grad);
+            if u*Norm<eps
                 break
             end
         
-            % pick the block with uniform probability
-            i = randi([1 u]);
-        
             % update
-            y(i) = y(i) - alpha*grad(i);
+            y(i) = y(i) - alpha*grad;
             time(k)=toc;
             accuracy(k)=1-sum(abs(sign(y)-y_exact)/u);
+            if mod(k,50)==0
+                Norms(k)=norm(f_deriv(y,y_samp,W,W_samp));
+            else
+                Norms(k)=Norms(k-1);
+            end
+
         end
     case 2 %armijo rule
-        for k = 1:maxit
+        for k = 2:maxit+1
             tic;
-
-            % condition to stop
-            grad = f_deriv(y,y_samp,W,W_samp);
-            
-            Norm = norm(grad);
-            Norms(k) = Norm;
-            
-            if Norm<eps
-                break
-            end
-            z = y;
             % pick the block with uniform probability
             i = randi([1 u]);
-            grad_z = f_deriv_block(z,y_samp,W,W_samp,i);
-            grad=zeros([u ,1]);
-            grad(i)=grad_z;
-            alpha=armijo_rule(delta, grad, y, y_samp, W, W_samp);
+            grad=f_deriv_block(y,y_samp,W,W_samp,i);
+            Norm = abs(grad);
+            if u*Norm<eps
+                break
+            end
+ 
+            alpha=armijo_rule(delta, C(i), y, y_samp, W, W_samp);
         
             % update
-            y(i) = y(i) - alpha*grad(i);
+            y(i) = y(i) - alpha*grad;
             time(k)=toc;
             accuracy(k)=1-sum(abs(sign(y)-y_exact)/u);
+
+            if mod(k,50)==0
+                Norms(k)=norm(f_deriv(y,y_samp,W,W_samp));
+            else
+                Norms(k)=Norms(k-1);
+            end
+
         end
     case 3 %exact line search        
-        for k = 1:maxit
+        for k = 2:maxit+1
             tic;
-
-            % condition to stop
-            grad = f_deriv(y,y_samp,W,W_samp);
-            
-            Norm = norm(grad);
-            Norms(k) = Norm;
-            
-            if Norm<eps
+            i = randi([1 u]);
+            grad=f_deriv_block(y,y_samp,W,W_samp,i);
+            Norm = abs(grad);
+            if u*Norm<eps & Norms(k-1)<eps*100
                 break
             end
-            z = y;
             % pick the block with uniform probability
-            i = randi([1 u]);
-            grad_z = f_deriv_block(z,y_samp,W,W_samp,i);
-            grad=zeros([u ,1]);
-            grad(i)=grad_z;
-            alpha=exact_line_search(y,y_samp,W,W_samp,grad);
+            alpha=exact_line_search(y,y_samp,W,W_samp,C(i));
         
             % update
-            y(i) = y(i) - alpha*grad(i);
+            y(i) = y(i) - alpha*grad;
             time(k)=toc;
             accuracy(k)=1-sum(abs(sign(y)-y_exact)/u);
+            if mod(k,50)==0
+                Norms(k)=norm(f_deriv(y,y_samp,W,W_samp));
+            else
+                Norms(k)=Norms(k-1);
+            end
         end
 end
        
@@ -86,4 +93,3 @@ for k=1:length(time)
 end
 end
 
-  
