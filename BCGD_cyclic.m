8f65285a55c923d8b30e0513c59206e453827f9c
GianmarcoLattaruolo <87989501+GianmarcoLattaruolo@users.noreply.github.com>
2022-05-07 14:24:26 +0200

    Update BCGD_cyclic.m

--- BCGD_cyclic.m
+++ BCGD_cyclic.m
@@ -1,102 +1,102 @@
 % Cyclic BCGD method
 
 function [y, timeVec, Norms, accuracy] = ...
-    BCGD_cyclic(alpha,y,maxit,eps,y_samp,W,W_samp,stepsize,y_exact,delta)
+    BCGD_cyclic(y,maxit,eps,y_samp,W,W_samp,stepsize,y_exact,delta)
 
 u = length(y);
+for i=1:u
+    W_l(i) = sum(W_samp(:,i));
+    W_u(i) = sum(W(:,i));
+end
+C=W_l+W_u;
 
 
 
 switch stepsize
     case 1
+        lambda_max = max(eig(W));
+        L=sqrt(max(C)^2+lambda_max^2);
+        alpha=1/L;
+
         for k = 1:maxit
             tic;
             % condition to stop
-            grad = f_deriv(y,y_samp,W,W_samp);
-           
-            Norm = norm(grad);
-            Norms(k) = Norm;
-            
-            if Norm<eps
-                break
-            end
-        
+            grad=zeros([u,1]);
+
             z = y;
-        
             for i = 1:u
-               
                 grad_z = f_deriv_block(z,y_samp,W,W_samp,i);
                 z(i) = z(i) - alpha.*grad_z;
-        
+                grad(i)=grad_z;
             end
         
             % update
             y = z;
             time(k)=toc;
             accuracy(k)=1-sum(abs(sign(y)-y_exact)/length(y));
+            
+            %stop condition
+            Norm=norm(grad);
+            Norms(k)=Norm;
+            if Norm<eps
+                break
+            end
+
         end 
         
     case 2  %armijo
         for k = 1:maxit
             tic;
-            % condition to stop
-            grad = f_deriv(y,y_samp,W,W_samp);
-           
-            Norm = norm(grad);
-            Norms(k) = Norm;
-            
-            if Norm<eps
-                break
-            end
-        
+            grad=zeros([u,1]);
+
             z = y;
-        
             for i = 1:u
                 grad_z = f_deriv_block(z,y_samp,W,W_samp,i);
-                grad=zeros([u ,1]);
                 grad(i)=grad_z;
-                alpha=armijo_rule(delta, grad, y, y_samp, W, W_samp);
+                alpha=armijo_rule(delta, C(i) , y, y_samp, W, W_samp);
                 z(i) = z(i) - alpha.*grad_z;
-        
             end
         
             % update
             y = z;
             time(k)=toc;
-            accuracy(k)=1-sum(abs(sign(y)-y_exact)/length(y));
+            accuracy(k)=1-sum(abs(sign(y)-y_exact)/u);
+
+            Norm=norm(grad);
+            Norms(k)=Norm;
+            if Norm<eps
+                break
+            end
         end 
+
     case  3 %exact line search
         for k = 1:maxit
             tic;
-            % condition to stop
-            grad = f_deriv(y,y_samp,W,W_samp);
-           
-            Norm = norm(grad);
-            Norms(k) = Norm;
-            
-            if Norm<eps
-                break
-            end
+            grad=zeros([u,1]);
         
-            z = y;
-        
+            z = y;        
             for i = 1:u
                 grad_z = f_deriv_block(z,y_samp,W,W_samp,i);
-                grad=zeros([u ,1]);
                 grad(i)=grad_z;
-                alpha=exact_line_search(y,y_samp,W,W_samp,grad);
+                alpha=exact_line_search(y,y_samp,W,W_samp,C(i));
                 z(i) = z(i) - alpha.*grad_z;
-        
             end
         
             % update
             y = z;
             time(k)=toc;
             accuracy(k)=1-sum(abs(sign(y)-y_exact)/length(y));
+
+            Norm=norm(grad);
+            Norms(k)=Norm;
+            if Norm<eps
+                break
+            end
+
         end 
+
 end
 for k=1:length(time)
     timeVec(k)=sum(time(1:k));
 end
 end
-
