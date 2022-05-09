07c26bb49da186dca8ccfb53f4515bc9dbc252f8
ir-ca <96775482+ir-ca@users.noreply.github.com>
2022-05-09 10:27:22 +0200

    Update f_deriv_block.m

--- f_deriv_block.m
+++ f_deriv_block.m
@@ -1,5 +1,15 @@
 function [grad] = f_deriv_block(y,y_samp,W,W_samp,j)
-% j = indice del blocco 1-dim
+
+% GRADIENT COMPUTATION FOR j-th BLOCK OF f
+
+% INPUTS
+% y: point in which calculate the gradient
+% y_samp: parameters of the loss fuction
+% w,W_samp: weights of the loss function
+% j: size 1 block index
+
+% OUTPUT
+% grad: j-th component of gradient vector of f
 
 sum1 = 0;
 sum2 = 0;
